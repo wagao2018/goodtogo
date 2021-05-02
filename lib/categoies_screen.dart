ebe7f64fa04ce2e9a2101c9b249e3a0a4023abad
wa gao <wagao@Wa-2019Macbook-Pro.local>
2021-05-02 01:09:50 -0700

    start thesis app

---
+++ lib/categoies_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatefulWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        
+      ], 
+    );
+  
+}
+
+}
